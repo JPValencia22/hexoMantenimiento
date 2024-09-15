2c363f640713f0c8f45f98849ddf711441f7a1b0
camian0 <camian6@outlook.com>
2024-09-15 18:06:28 -0500

    fix patter to regex const was simplified
    the regex patter for verify ulr was modified to reduce their complexity.

--- lib/plugins/tag/link.ts
+++ lib/plugins/tag/link.ts
@@ -1,19 +1,20 @@
-import { htmlTag } from 'hexo-util';
+import { htmlTag } from "hexo-util";
 
-const rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)/;
+// const rUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\w]*))?)/;
 
+const rUrl = /([a-zA-Z]+:\/\/)?([\w.-]+)(\/[\w/.-]*)?(\?[\w=&%.-]*)?(#[\w-]*)?/;
 /**
-* Link tag
-*
-* Syntax:
-*   {% link text url [external] [title] %}
-*/
+ * Link tag
+ *
+ * Syntax:
+ *   {% link text url [external] [title] %}
+ */
 
 function linkTag(args: string[]) {
-  let url = '';
+  let url = "";
   const text = [];
   let external = false;
-  let title = '';
+  let title = "";
   let i = 0;
   const len = args.length;
 
@@ -37,21 +38,21 @@ function linkTag(args: string[]) {
   if (args.length) {
     const shift = args[0];
 
-    if (shift === 'true' || shift === 'false') {
-      external = shift === 'true';
+    if (shift === "true" || shift === "false") {
+      external = shift === "true";
       args.shift();
     }
 
-    title = args.join(' ');
+    title = args.join(" ");
   }
 
   const attrs = {
     href: url,
     title,
-    target: external ? '_blank' : ''
+    target: external ? "_blank" : "",
   };
 
-  return htmlTag('a', attrs, text.join(' '));
+  return htmlTag("a", attrs, text.join(" "));
 }
 
 export = linkTag;
